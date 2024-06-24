406fa46a7bd6c95ccb0d56da33428dedd06a9aa8
= <24886321+itsjustmustafa@users.noreply.github.com>
2024-06-24 22:44:00 +1000

    Formatting changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
-import jsonData from './data/what_can_i_run_data_april_2024.json';
-import Game from './types';
-import GameTable from './GameTable';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import jsonData from "./data/what_can_i_run_data_april_2024.json";
+import Game from "./types";
+import GameTable from "./GameTable";
 
 const App: React.FC = () => {
   const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
     setGames(jsonData.results);
-  }, [])
+  }, []);
 
   const goToMyGitHub = () => {
     window.open("https://github.com/itsjustmustafa/what-games-can-mazza-run");
-  }
+  };
 
   const handleSearch = (query: string) => {
-    const filteredGames = jsonData.results.filter(game =>
+    const filteredGames = jsonData.results.filter((game) =>
       game.game.toLowerCase().includes(query.toLowerCase())
     );
     setGames(filteredGames);
-  }
+  };
   const LogoImage = () => (
-    <img src="/headphone_hmm.svg"
-      className='logo'
-      onClick={goToMyGitHub} />
+    <img src="/headphone_hmm.svg" className="logo" onClick={goToMyGitHub} />
   );
 
   return (
     <div id="app">
-      <div id="header"
-        style={{ display: "flex", placeContent: "center", alignItems: "center" }}>
+      <div
+        id="header"
+        style={{
+          display: "flex",
+          placeContent: "center",
+          alignItems: "center",
+        }}
+      >
         <LogoImage />
-        <h1 style={{ display: "inline" }}>
-          What games can Mazza run?
-        </h1>
+        <h1 style={{ display: "inline" }}>What games can Mazza run?</h1>
         <LogoImage />
       </div>
 
       <GameTable games={games} onSearch={handleSearch} />
-    </div >
+    </div>
   );
-}
+};
 
-export default App
+export default App;
--- src/GameTable.tsx
+++ src/GameTable.tsx
@@ -1,116 +1,134 @@
 import React, { useEffect, useState, useRef } from "react";
-import Game from './types';
-import SearchBar from './SearchBar';
-
+import Game from "./types";
+import SearchBar from "./SearchBar";
 
 interface GameTableProp {
-    games: Game[],
-    onSearch: (query: string) => void;
+  games: Game[];
+  onSearch: (query: string) => void;
 }
 
 const getGameResult = (game: Game) => {
-    return game.minResult === "FAIL" ? 'none' : game.recResult !== "FAIL" ? 'recommended' : 'minimum';
-}
+  return game.minResult === "FAIL"
+    ? "none"
+    : game.recResult !== "FAIL"
+      ? "recommended"
+      : "minimum";
+};
 
 const getPassSymbol = (game: Game) => {
-    switch (getGameResult(game)) {
-        case 'recommended': return 'Recommended specs'
-        case 'minimum': return 'Minimum specs'
-        default: return 'Nope'
-    }
-}
+  switch (getGameResult(game)) {
+    case "recommended":
+      return "Recommended specs";
+    case "minimum":
+      return "Minimum specs";
+    default:
+      return "Nope";
+  }
+};
 const getResultStyleClass = (game: Game) => {
-    switch (getGameResult(game)) {
-        case 'recommended': return 'result-rec'
-        case 'minimum': return 'result-min'
-        default: return 'result-fail'
-    }
-}
+  switch (getGameResult(game)) {
+    case "recommended":
+      return "result-rec";
+    case "minimum":
+      return "result-min";
+    default:
+      return "result-fail";
+  }
+};
 const GameTable: React.FC<GameTableProp> = ({ games, onSearch }) => {
-    const defaultTotalLoaded = 100;
-    const loadIncrement = 50;
-    const [totalLoaded, _setTotalLoaded] = useState<number>(defaultTotalLoaded);
-    const totalLoadedRef = useRef(totalLoaded);
-    const setTotalLoaded = (totalLoadedUpdate: (prevTotalLoaded: number) => number) => {
-        totalLoadedRef.current = totalLoadedUpdate(totalLoadedRef.current);
-        _setTotalLoaded(totalLoadedUpdate);
+  const defaultTotalLoaded = 100;
+  const loadIncrement = 50;
+  const [totalLoaded, _setTotalLoaded] = useState<number>(defaultTotalLoaded);
+  const totalLoadedRef = useRef(totalLoaded);
+  const setTotalLoaded = (
+    totalLoadedUpdate: (prevTotalLoaded: number) => number
+  ) => {
+    totalLoadedRef.current = totalLoadedUpdate(totalLoadedRef.current);
+    _setTotalLoaded(totalLoadedUpdate);
+  };
+
+  const gamesLengthRef = useRef(games.length);
+
+  useEffect(() => {
+    const bottomRowElement = document.getElementById("end-of-table");
+    const observerOptions = {
+      threshold: 1.0,
     };
-
-    const gamesLengthRef = useRef(games.length);
-
-    useEffect(() => {
-        const bottomRowElement = document.getElementById("end-of-table");
-        const observerOptions = {
-            threshold: 1.0
-        }
-        const observer = new IntersectionObserver((entry) => {
-            if (entry[0].isIntersecting) {
-                incrementTotalLoaded();
-            }
-        }, observerOptions);
-        if (bottomRowElement) {
-            observer.observe(bottomRowElement);
-        }
-        return () => {
-            if (bottomRowElement) {
-                observer.unobserve(bottomRowElement);
-            }
-        }
-    }, []);
-
-    const sortedGames = games.slice().sort(
-        (a, b) => b.PopCount - a.PopCount
-    ).slice(0, totalLoadedRef.current);
-
-    const incrementTotalLoaded = () => {
-        if (gamesLengthRef.current > totalLoadedRef.current) {
-            setTotalLoaded(prevTotalLoaded => prevTotalLoaded + loadIncrement);
-        }
+    const observer = new IntersectionObserver((entry) => {
+      if (entry[0].isIntersecting) {
+        incrementTotalLoaded();
+      }
+    }, observerOptions);
+    if (bottomRowElement) {
+      observer.observe(bottomRowElement);
     }
+    return () => {
+      if (bottomRowElement) {
+        observer.unobserve(bottomRowElement);
+      }
+    };
+  }, []);
 
-    useEffect(() => {
-        setTotalLoaded(_ => Math.min(defaultTotalLoaded, games.length));
-        gamesLengthRef.current = games.length;
-    }, [games])
+  const sortedGames = games
+    .slice()
+    .sort((a, b) => b.PopCount - a.PopCount)
+    .slice(0, totalLoadedRef.current);
 
-    return (
-        <>
-            <table id="gametable">
-                <thead>
-                    <tr className="searchBarRow">
-                        <th colSpan={3}><SearchBar onSearch={onSearch} /></th>
-                    </tr>
-                    <tr className="titleRow">
-                        <th>Game</th>
-                        <th>Can I?</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {sortedGames.map(game => (
-                        <tr key={game.id}>
-                            <td
-                                style={{ width: "50%" }}
-                            >{game.game}</td>
-                            <td
-                                style={{ width: "50%" }}
-                                className={"result " + getResultStyleClass(game)}>
-                                {getPassSymbol(game)}
-                            </td>
-                        </tr>
-                    ))}
-                    <tr>
-                        {(games.length > totalLoaded) && <td colSpan={3} id="increment" onClick={incrementTotalLoaded}>
-                            (Show more...)
-                        </td>}
-                    </tr>
-                    {(games.length == 0) && <td colSpan={3}> No games found... </td>}
-                </tbody>
-            </table>
-            <div id="end-of-table" style={{
-                "height": "0px",
-            }}></div>
-        </>
-    );
-}
+  const incrementTotalLoaded = () => {
+    if (gamesLengthRef.current > totalLoadedRef.current) {
+      setTotalLoaded((prevTotalLoaded) => prevTotalLoaded + loadIncrement);
+    }
+  };
+
+  useEffect(() => {
+    setTotalLoaded((_) => Math.min(defaultTotalLoaded, games.length));
+    gamesLengthRef.current = games.length;
+  }, [games]);
+
+  return (
+    <>
+      <table id="gametable">
+        <thead>
+          <tr className="searchBarRow">
+            <th colSpan={3}>
+              <SearchBar onSearch={onSearch} />
+            </th>
+          </tr>
+          <tr className="titleRow">
+            <th>Gamea</th>
+            <th>Can I?</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedGames.map((game) => (
+            <tr key={game.id}>
+              <td style={{ width: "50%" }}>{game.game}</td>
+              <td
+                style={{ width: "50%" }}
+                className={"result " + getResultStyleClass(game)}
+              >
+                {getPassSymbol(game)}
+              </td>
+            </tr>
+          ))}
+          <tr>
+            {games.length > totalLoaded && (
+              <td colSpan={3} id="increment" onClick={incrementTotalLoaded}>
+                (Show more...)
+              </td>
+            )}
+          </tr>
+          {games.length == 0 && <td colSpan={3}> No games found... </td>}
+        </tbody>
+      </table>
+      <div
+        id="end-of-table"
+        style={{
+          height: "0px",
+        }}
+      ></div>
+    </>
+  );
+};
 
 export default GameTable;
